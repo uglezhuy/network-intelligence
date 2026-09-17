3d31fc0e9e718403cda1abb6953757c13c192a13
slava <aleshin03slava@gmail.com>
2026-09-17 17:22:51 +0300

    feat: api: маршруты /api/scan/ и /api/stopMonitor/, вынос обработки запросов в routes.ts

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,21 +1,39 @@
 import { IncomingMessage, ServerResponse } from "node:http";
 import { showMonitorsByTelegramUserId } from "../telegram/selectMonitorsByTelegramUserId.js";
+import { analyzers } from "../analyzers.js";
+import { saveResultinScan } from "../database/results.js";
+import { stopMonitorID } from "../stopMonitor.js"
+
+
 
 async function handleApiRequest(
     req: IncomingMessage,
     res: ServerResponse
 ) {
 
-    //////////////////////////////////////////////////////////////////
+
+
+    const TEST_TELEGRAM_USER_ID = 503362430; //временный тг айди  для тестов
+
+
+
+
+    console.log("REQUEST:", req.method, req.url);
+
     res.setHeader("Access-Control-Allow-Origin", "*");
 
+    // /api/monitors//////////////////////////////////////////////////////////////////////////////
     if (
         req.method === "GET" &&
         req.url?.startsWith("/api/monitors/")
     ) {
+        console.log("ROUTE: /api/monitors/");
+
         const telegramUserId = Number(
             req.url.split("/api/monitors/")[1]
         );
+
+        console.log("Telegram User ID:", telegramUserId);
 
         if (!Number.isInteger(telegramUserId)) {
             res.writeHead(400, {
@@ -53,6 +71,161 @@ async function handleApiRequest(
         return;
     }
 
+    // /api/scan/ //////////////////////////////////////////////////////////////////////////////
+    console.log("Проверяем scan route");
+    console.log("Method:", req.method);
+    console.log("URL:", req.url);
+    console.log(
+        "startsWith /api/scan/:",
+        req.url?.startsWith("/api/scan/")
+    );
+
+    if (
+        req.method === "GET" &&
+        req.url?.startsWith("/api/scan/")
+    ) {
+        console.log("ROUTE: /api/scan/");
+
+        const target =
+            req.url.split("/api/scan/")[1];
+
+        console.log("Target:", target);
+
+        if (!target) {
+            res.writeHead(400, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify({
+                error: "Некорректный URL"
+            }));
+
+            return;
+        }
+
+        try {
+            console.log("Запускаем analyzers:", target);
+
+            const scanResult =
+                await analyzers(target);
+
+            console.log("Сканирование завершено");
+            await saveResultinScan(scanResult, TEST_TELEGRAM_USER_ID);
+            console.log("Результат сохранен в базе данных");
+            res.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify(scanResult));
+        } catch (error) {
+            console.error("Ошибка API:", error);
+
+            res.writeHead(500, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify({
+                error: "Ошибка сервера"
+            }));
+        }
+
+        return;
+    }
+
+
+
+
+
+
+
+
+    //остановка //////////////////////////////////////////////////////////////////////////////
+    if (
+        req.method === "GET" &&
+        req.url?.startsWith("/api/stopMonitor/")
+    ) {
+        console.log("ROUTE: /api/stopMonitor/");
+
+        const target =
+            req.url.split("/api/stopMonitor/")[1];
+
+        console.log("Target:", target);
+
+        if (!target) {
+            res.writeHead(400, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify({
+                error: "Некорректный URL"
+            }));
+
+            return;
+        }
+
+        try {
+            console.log("Запускаем stopMonitorID:", target);
+
+
+            await stopMonitorID(target);
+
+            console.log("Отсановка завершено");
+
+        } catch (error) {
+            console.error("Ошибка API:", error);
+
+            res.writeHead(500, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify({
+                error: "Ошибка сервера"
+            }));
+        }
+
+        return;
+    }
+    ///////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // неизвестный маршрут ///////////////////////////////////////////////////////////////////////////////////
+    console.log("ROUTE NOT FOUND");
+
     res.writeHead(404, {
         "Content-Type": "application/json"
     });
@@ -60,29 +233,6 @@ async function handleApiRequest(
     res.end(JSON.stringify({
         error: "Маршрут не найден"
     }));
-
-    ////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export { handleApiRequest };
--- src/api/startApiServer.ts
+++ src/api/startApiServer.ts
@@ -1,67 +1,16 @@
 import { createServer } from "node:http";
-import { showMonitorsByTelegramUserId } from "../telegram/selectMonitorsByTelegramUserId.js";
+import { handleApiRequest } from "./routes.js";
 
 function startApiServer() {
     const server = createServer(async (req, res) => {
-
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        if (req.method === "GET" && req.url?.startsWith("/api/monitors/")) {
-
-            const telegramUserId = Number(
-                req.url.split("/api/monitors/")[1]
-            );
-
-            if (!Number.isInteger(telegramUserId)) {
-                res.writeHead(400, {
-                    "Content-Type": "application/json"
-                });
-
-                res.end(JSON.stringify({
-                    error: "Некорректный Telegram User ID"
-                }));
-
-                return;
-            }
-
-            try {
-                const monitors = await showMonitorsByTelegramUserId(
-                    telegramUserId
-                );
-
-                res.writeHead(200, {
-                    "Content-Type": "application/json"
-                });
-
-                res.end(JSON.stringify(monitors));
-
-            } catch (error) {
-                console.error("Ошибка API:", error);
-
-                res.writeHead(500, {
-                    "Content-Type": "application/json"
-                });
-
-                res.end(JSON.stringify({
-                    error: "Ошибка сервера"
-                }));
-            }
-
-            return;
-        }
-
-        res.writeHead(404, {
-            "Content-Type": "application/json"
-        });
-
-        res.end(JSON.stringify({
-            error: "Маршрут не найден"
-        }));
+        await handleApiRequest(req, res);
     });
 
     server.listen(3000, () => {
         console.log("API server started: http://localhost:3000");
     });
 }
+
 startApiServer();
 
 export { startApiServer };
